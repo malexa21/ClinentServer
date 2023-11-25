6e889de6fc26d3836c6ac58a5dd1e18c14332ea4
malexa21 <aaliroalexandra@gmail.com>
2023-11-25 17:16:03 +0200

    Main code

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const URL =
+  "https://air-quality-api.open-meteo.com/v1/air-quality?latitude=52.52&longitude=13.41&hourly=pm10,pm2_5";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const tableBody = document.getElementById("tableBody");
+const options = {
+  weekday: "long",
+  hour: "numeric",
+}
+const dateFormatter = new Intl.DateTimeFormat("ro-RO" , options);
 
-setupCounter(document.querySelector('#counter'))
+
+fetch(URL, {method: "GET"})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+
+    for (let i = 0; i < data.hourly.time.length; i++) {
+      const row = document.createElement("tr");
+
+
+      const timeCell = document.createElement("td");
+      timeCell.innerText = dateFormatter.format(new Date(data.hourly.time[i]));
+      row.appendChild(timeCell);
+
+      const quality25Cell = document.createElement("td");
+      quality25Cell.innerText = data.hourly.pm2_5[i];
+      row.appendChild(quality25Cell);
+
+      const quality10Cell = document.createElement("td");
+      quality10Cell.innerText = data.hourly.pm10[i];
+      row.appendChild(quality10Cell);
+
+  
+      tableBody.appendChild(row);
+    };
+
+  });
+
+
+
